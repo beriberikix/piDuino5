b9fc76897cadb9254b3e0d644802568b6618bc22
root <root@raspberrypi.(none)>
2013-06-26 05:06:08 +0000

    Trying to communicate directly over serial pins

--- app.js
+++ app.js
@@ -7,44 +7,68 @@ var five = require("johnny-five"),
     path = require('path'),
     localtunnel = require('localtunnel'),
     request = require('request'),
+    os = require('os'),
     hbridge,
-    board;
+    board,
+    client;
 
-board = new five.Board();
+board = new five.Board({ port: "/dev/ttyAMA0" });
 
 app.configure(function(){
   app.use(express.static('public'));
 });
 
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('onBoardReady', { ready : true });
+  socket.on('forward', function(data) {
+    hbridge.forward();
+  });
+  socket.on('reverse', function(data) {
+    hbridge.reverse();
+  });
+  socket.on('gee', function(data) {
+    hbridge.gee();
+  });
+  socket.on('haw', function(data) {
+    hbridge.haw();
+  });
+  socket.on('halt', function(data) {
+    hbridge.halt();
+  });
+ });
+
 board.on("ready", function() {
   // Create a new `hbridge` hardware instance.
   hbridge = new five.HBridge({
     "right": {
-	"forward": 7,
-	"reverse": 12
+      "forward": 7,
+      "reverse": 12
     },
     "left": {
-        "forward": 8,
-        "reverse": 13
+      "forward": 8,
+      "reverse": 13
     }
   });
 
   // Inject the `hbridge` hardware into
   // the Repl instance's context;
-  // allows direct command line access
   board.repl.inject({
     hbridge: hbridge
   });
 
-  // starting web server
-  server.listen(1337);
-
-var client = localtunnel.connect({
+client = localtunnel.connect({
     // the localtunnel server
     host: 'http://localtunnel.me',
     // your local application port
     port: 1337
 });
+
+  // starting web server
+  server.listen(1337);
 
 // when your are assigned a url
 client.on('url', function(url) {
@@ -56,27 +80,4 @@ client.on('url', function(url) {
 
   request.post(dhd_url);
 });
-
-  app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-  });
-
-  io.sockets.on('connection', function (socket) {
-    socket.emit('onBoardReady', { ready : true });
-    socket.on('forward', function(data) {
-      hbridge.forward();
-    });
-    socket.on('reverse', function(data) {
-      hbridge.reverse();
-    });
-    socket.on('gee', function(data) {
-      hbridge.gee();
-    });
-    socket.on('haw', function(data) {
-      hbridge.haw();
-    });
-    socket.on('halt', function(data) {
-      hbridge.halt();
-    });
-  });
 });
